ad56a33b7890a6d678efc8aa7d9d18286f29d8cf
Michael Beutler <michael.beutler@iperka.com>
2022-01-31 13:57:30 +0100

    Added GitHub workflows. 🎈

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 "use strict";
 
-import clear from 'rollup-plugin-clear';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import screeps from 'rollup-plugin-screeps';
+import clear from "rollup-plugin-clear";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import screeps from "rollup-plugin-screeps";
+
+require('dotenv').config()
 
 let cfg;
 const dest = process.env.DEST;
@@ -12,6 +14,8 @@ if (!dest) {
   console.log("No destination specified - code will be compiled but not uploaded");
 } else if ((cfg = require("./screeps.json")[dest]) == null) {
   throw new Error("Invalid upload destination");
+} else {
+  cfg.token = process.env.SCREEPS_AUTH_TOKEN;
 }
 
 export default {
@@ -26,7 +30,7 @@ export default {
     clear({ targets: ["dist"] }),
     resolve({ rootDir: "src" }),
     commonjs(),
-    typescript({tsconfig: "./tsconfig.json"}),
-    screeps({config: cfg, dryRun: cfg == null})
+    typescript({ tsconfig: "./tsconfig.json" }),
+    screeps({ config: cfg, dryRun: cfg == null })
   ]
-}
+};
