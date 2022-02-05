02826b182b7ae65192cb62a41ac281614f7de577
Michael Beutler <michael.beutler@iperka.com>
2022-02-05 10:17:28 +0100

    Added new helper functions. 🏗

--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -1,3 +1,12 @@
 export function randomIndex(length: number) {
   return Math.floor(Math.random() * length);
 }
+
+export function getPointsAroundPoint(x: number, y: number, room: Room): LookAtResultWithPos<LookConstant>[] {
+  return room.lookAtArea(y + 1, x + 1, y - 1, y - 1, true);
+}
+
+export function getAvailableSourcePlaces(source: Source) {
+  const points = getPointsAroundPoint(source.pos.x, source.pos.y, source.room);
+  return points.filter(point => point.type === "terrain" && point.terrain === "plain");
+}
